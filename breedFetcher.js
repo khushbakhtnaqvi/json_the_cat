e3a7f4934904bfd75e3eb965925535628b8d3920
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-11-04 18:44:34 -0400

    refactored code and implemented mocha tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,21 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const name = args[0];
 let data;
-request(`https://api.thecatapi.com/v1/breeds/search?name=${name}`, (error, response, body) => {
-  if (error) {
-    console.log("Request error:", error);
-    return;
-  }
-  data = JSON.parse(body);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?name=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error,null);
+      return;
+    }
+    data = JSON.parse(body);
+  
+    if (data[0]) {
+      callback(null,data[0]['description']);
+    } else {
+      let desc = "Breed Not Found";
+      callback(null,desc);
+    }
+  });
+};
 
-  if (data[0]) {
-    const res = data[0]['description'];
-    console.log("Description:", res);
-  } else {
-    console.log("Breed Not Found");
-  }
-});
+
+module.exports = {fetchBreedDescription};
